b7e7893e9bce2ae2540bc39feaa8aa9f38709048
unknown <eagle@.(none)>
2008-08-22 23:00:44 +0800

    update

---
+++ script/notice_runner.rb
@@ -0,0 +1,30 @@
+current_time = Time.now
+
+User.find(:all,:order => " updated_at desc ").each do |user|
+  begin
+    notifications = [
+                    "<a href='http://apps.xiaonei.com/beautypk/homes/show'>诚邀您安装美容达人PK赛，这里有你不知道的美容问题，并且有大牌的化妆品赠送！</a>"
+                    ]
+    res_note = user.xn_session.invoke_method("xiaonei.notifications.send", 
+                                                    :to_ids => [user.xid].join(","), 
+                                                    :notification => notifications.rand)
+    
+   # res_feed = user.xn_session.invoke_method("xiaonei.feed.publishTemplatizedAction", 
+    #                                                :title_data => { 
+     #                                               }.to_json,
+      #                                              :body_data => { 
+       #                                               :my_uid => user.xid,
+         #                                             :age => user.age,
+        #                                            }.to_json,
+          #                                          :template_id => 1)
+    
+    puts "#{current_time}: process user #{user.id}:  #{res_note.inspect}"
+    
+    user.sent = true
+    user.save
+  rescue Exception => exp
+    puts "exp: #{exp.inspect}"
+  end
+end
+
+
