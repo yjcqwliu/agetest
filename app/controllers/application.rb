dedd96a413488d7153d673ce6b233f963ec9e7b4
unknown <eagle@.(none)>
2008-09-07 16:03:28 +0800

    update

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -23,15 +23,15 @@ class ApplicationController < ActionController::Base
         @current_user.save
       end
     end
-	if @current_user.friend_ids.nil? or @current_user.friend_ids.size == 0 or @current_user.updated_at > (Time.now - 48.hour)
-        res = xiaonei_session.invoke_method("xiaonei.friends.get")
-        if res.kind_of? Xiaonei::Error
-          @current_user.friend_ids = [] if @current_user.friend_ids.empty?
-        else
-          @current_user.friend_ids = res
-        end
-        @current_user.save
-	 end 
+	#if @current_user.friend_ids.nil? or @current_user.friend_ids.size == 0 or @current_user.updated_at > (Time.now - 48.hour)
+    #    res = xiaonei_session.invoke_method("xiaonei.friends.get")
+    #    if res.kind_of? Xiaonei::Error
+    #      @current_user.friend_ids = [] if @current_user.friend_ids.empty?
+    #    else
+    #      @current_user.friend_ids = res
+    #    end
+    #    @current_user.save
+	# end 
 
   end
   
