543cda6b52324a9bc3d228aff6e727de293315e2
unknown <Administrator@.(none)>
2008-10-26 23:36:20 +0800

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :messages
+	serialize :friend_ids
  def xn_session
     @xn_session ||= Xiaonei::Session.new("xn_sig_session_key" => session_key, "xn_sig_user" => xid)
   end
